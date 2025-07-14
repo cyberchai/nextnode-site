d0f6f2cc4cf39bd1b759409470e86e210c0c4b5c
cyberchai <chairachananharder@gmail.com>
2025-07-13 21:01:35 -0400

    please don't break

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
-	title: `NextNode LLC | Digital Product Design & Development`,
-	description: `At NextNode, we design and build thoughtful digital products with clean design, strong security, and clear purpose.`,
+	title: `NextNode LLC | Software Development`,
+	description: `At NextNode, we build thoughtful digital products with clean design, strong security, and clear purpose.`,
 	url: `https://nextnode.ventures`, // No trailing slash!
 	name: `NextNode`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
