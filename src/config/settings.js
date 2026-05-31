3d7101e0411177847b81b08e69484aad89569cb4
charder <chairachananharder@gmail.com>
2026-05-30 23:26:58 -0400

    studio add

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `NextNode LLC | Software Development`,
-	description: `At NextNode, we build thoughtful digital products with clean design, strong security, and clear purpose.`,
+	title: `NextNode Studio | Software Development`,
+	description: `At NextNode Studio, we build thoughtful digital products with clean design, strong security, and clear purpose.`,
 	url: `https://nextnode.ventures`, // No trailing slash!
-	name: `NextNode`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `NextNode LLC`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
 	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
